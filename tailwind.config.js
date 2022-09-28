ac9ef9cc2714020a081fc29ac372db7ab8f052c7
Drew Johnston <drew@johnston.us>
2022-09-28 14:12:11 -0500

    Styling updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./**/*.htm"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: {
+          200: '#e3d9cc',
+          300: '#d6c7b4',
+          400: '#cbb9a0',
+          500: '#bca485',
+          600: '#B49976',
+          700: '#b39874',
+          800: '#aa8b63',
+          900: '#9d7e56'
+        }
+      }
+    },
   },
   plugins: [],
 }
